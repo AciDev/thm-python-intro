7b36319890053fd99dcaf4ec90462a61d2274b7e
Thomas Gomer <aciwebdev@gmail.com>
2020-06-02 22:07:57 +0100

    Updated the project to be more functional

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
-from base64 import *
+from base64 import b16decode, b32decode, b64decode, b16encode, b32encode, b64encode
+import codecs
+
+
+def isBase16(sb):
+    try:
+        if isinstance(sb, str):
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return b16encode(b16decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
+
+
+def isBase32(sb):
+    try:
+        if isinstance(sb, str):
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return b32encode(b32decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
+
+
+def isBase64(sb):
+    try:
+        if isinstance(sb, str):
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return b64encode(b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
+
+
+def detectBaseEncoding(s):
+    if(isBase16(s)):
+        return b16decode(s)
+    elif(isBase32(s)):
+        return b32decode(s)
+    elif(isBase64(s)):
+        return b64decode(s)
+    else:
+        return False
 
 
 def main():
     file = open('./encodedflag.txt', 'r').read()
-
-    for i in range(15):
-        if i < 5:
-            file = b16decode(file)
-        elif i < 10:
-            file = b32decode(file)
+    decoding = detectBaseEncoding(file)
+    while decoding != False:
+        decoding = detectBaseEncoding(decoding)
+        if type(decoding) == bytes:
+            flag = decoding
         else:
-            file = b64decode(file)
+            flag = flag
 
-    print(file.decode())
+    print(flag.decode())
 
 
 if __name__ == "__main__":
